6bc32981f9496e8782a41c6a9142681bda5bc547
RKChrist <76455111+RKChrist@users.noreply.github.com>
2021-06-04 01:09:33 +0200

    Comments on without test driven development (testing later with test driven development).

--- TestDrivenDevelopment/Program.cs
+++ TestDrivenDevelopment/Program.cs
@@ -11,10 +11,13 @@ namespace TestDrivenDevelopment
             Console.WriteLine("Write a Number");
             string number = Console.ReadLine();
             RomanNumberGenerator rng = new RomanNumberGenerator();
+            RomanNumbersToINTGenerator rntig = new RomanNumbersToINTGenerator();
             try
             {
                 var romanNumbers = rng.ConvertToRomanNumbers(number);
                 Console.WriteLine(romanNumbers);
+                var normalnumbers = rntig.ConvertToInt(romanNumbers);
+                Console.WriteLine(normalnumbers);
             }
             catch (ArgumentException ex)
             {
